75630e8353a97bda5b27366bf06e77f8af2572b2
Harshp61 <pandeyh477@gmail.com>
2025-10-01 15:56:54 +0530

    Fixed text-to-speech file generation and folder handling

--- reelit/template/generate.py
+++ reelit/template/generate.py
@@ -12,13 +12,17 @@ if __name__ == "__main__":
         # Create an empty done.txt if it doesn't exist
         with open("done.txt", "w") as f:
             pass
-
+        
     # Read the contents of done.txt
     with open("done.txt", "r") as f:
         done_folders = f.readlines()
+
+    done_folders = [line.strip() for line in done_folders]
 
     folders = os.listdir("user_uploads")
     for folder in folders:
         if folder not in done_folders:
             text_to_speech(folder)
             create_reel(folder)
+            with open("done.txt", "a") as f:
+                f.write(folder + "\n")
